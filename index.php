02a52896150d15172f812c0dd54411bef0e006ab
Abhinav Singh <113774414+codesabhinav@users.noreply.github.com>
2023-08-09 23:24:20 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+	        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+	        
+
+	
+	<title>My Resume</title>
+</head>
+<body>
+
+	<header>
+		<h2 class="logo">My Portfolio</h2>
+		<nav class="navigation">
+			<a href="#" class="active">Home</a>
+			<a href="#">About</a>
+			<a href="#">Service</a>
+			<a href="#">Contact</a>
+		</nav>
+	</header>
+
+	<section class="parallax">
+		
+		<img src="hill1.png" id="hill1">
+		<img src="hill2.png" id="hill2">
+		<img src="hill3.png" id="hill3">
+		<img src="hill4.png" id="hill4">
+		<img src="hill5.png" id="hill5">
+		<img src="tree.png" id="tree">
+		
+		<img src="leaf.png" id="leaf">
+		<h2 id="text">Abhinav Singh</h2>
+		<img src="plant.png" id="plant">
+	</section>
+
+	<section class="sec">
+		<h2>Professional Summmary</h2>
+		<p>I am highly passionate so if you are looking for a true passionate, self-teaching,
+ 			responsible person who loves to resolve problems and learn new things and have a
+ 			greeat adaptability. I can work independently and respect a deadline. I feel comfortable
+ 			working with a good team of developers and I have much patience while handling
+ 			difficult task...</p>
+
+		<h2>Languages and Frameworks</h2><br>
+			<div class="skills-bar">
+				<div class="row">
+					<div class="col-6">
+				<div class="bar">
+					<div class="info">
+						<span>HTML5</span>
+					</div>
+					<div class="progress-line html"><span></span></div>
+				</div>
+			</div>
+				<div class="col-6">
+				<div class="bar">
+					<div class="info">
+						<span>CSS3</span>
+					</div>
+					<div class="progress-line css"><span></span></div>
+				</div>
+			</div>
+		</div>
+				<div class="row">
+					<div class="col-6">
+				<div class="bar">
+					<div class="info">
+						<span>Javascript</span>
+					</div>
+					<div class="progress-line javascript"><span></span></div>
+				</div>
+			</div>
+
+				<div class="col-6">
+				<div class="bar">
+					<div class="info">
+						<span>Boostrap</span>
+					</div>
+					<div class="progress-line boostrap"><span></span></div>
+				</div>
+			</div>
+		</div>
+				<div class="row">
+					<div class="col-6">
+				<div class="bar">
+					<div class="info">
+						<span>SQL+MYSQL</span>
+					</div>
+					<div class="progress-line sql"><span></span></div>
+				</div>
+			</div>
+
+				<div class="col-6">
+				<div class="bar">
+					<div class="info">
+						<span>Jquery</span>
+					</div>
+					<div class="progress-line jquery"><span></span></div>
+				</div>
+			</div>
+		</div>
+				<div class="row">
+					<div class="col-6">
+				<div class="bar">
+					<div class="info">
+						<span>PHP</span>
+					</div>
+					<div class="progress-line php"><span></span></div>
+				</div>
+			</div>
+
+				<div class="col-6">
+				<div class="bar">
+					<div class="info">
+						<span>Fudamentals of Laravel</span>
+					</div>
+					<div class="progress-line laravel"><span></span></div>
+				</div>
+			</div>
+		</div>
+				<div class="row">
+					<div class="col-6">
+				<div class="bar">
+					<div class="info">
+						<span>C & C++</span>
+					</div>
+					<div class="progress-line c"><span></span></div>
+				</div>
+			</div>
+				<div class="col-6">
+				<div class="bar">
+					<div class="info">
+						<span>Basis of API</span>
+					</div>
+					<div class="progress-line api"><span></span></div>
+				</div>
+			</div>
+		</div>
+	</div><br><br>
+
+	<h2>Relevant Projects</h2><br>
+	<div class="row">
+		<div class="col-3">
+			<div class="card">
+  				<img src="student.png" class="card-img-top" alt="...">
+  				<div class="card-body">
+    				<div class="card-title">Students Management System(PHP)</div>
+    
+    				<div class="card-btn"><a href="www.google.com">Explore it</a></div>
+  				</div>
+			</div>
+	</div>
+
+<div class="col-3">
+	<div class="card">
+  <img src="employee.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <div class="card-title">Employee Management System(PHP)</div>
+    
+    <div class="card-btn"><a href="www.google.com">Explore it</a></div>
+  </div>
+</div>
+</div>
+
+
+<div class="col-3">
+	<div class="card">
+  <img src="cal.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <div class="card-title">Calculator (HTML,CSS & Javascript)</div>
+    
+    <div class="card-btn"><a href="www.google.com">Explore it</a></div>
+  </div>
+</div>
+</div>
+
+<div class="col-3">
+	<div class="card">
+  <img src="snake.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <div class="card-title">Snake Game (HTML,CSS & Javascript)</div>
+    <div class="card-btn"><a href="www.google.com">Explore it</a></div>
+  </div>
+</div>
+</div>
+</div>
+<div class="mb-3"></div>
+
+
+<div class="row">
+<div class="col-3">
+	<div class="card">
+  <img src="spr.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <div class="card-title">Rock Paper Scissor Game (HTML,CSS & Javascript)</div>
+    
+    <div class="card-btn"><a href="www.google.com">Explore it</a></div>
+  </div>
+</div>
+</div>
+
+<div class="col-3">
+	<div class="card">
+  <img src="ttt.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <div class="card-title">Rock Paper Scissor Game (HTML,CSS & Javascript)</div>
+    
+    <div class="card-btn"><a href="www.google.com">Explore it</a></div>
+  </div>
+</div>
+</div>
+
+</div>
+<div class="mb-5"></div>
+
+<h2>Education and Qualifications</h2><div class="mb-3"></div>
+
+<div class="contents">
+	<div class="box">
+		<h4>2018-2019</h4>
+		<h3>Secondary School</h3>
+		<p>Oxford English Higher Secondary School</p>
+	</div>
+
+	<div class="box">
+		<h4>2019-2020</h4>
+		<h3>Higher School</h3>
+		<p>Oxford English Higher Secondary School</p>
+	</div>
+
+	<div class="box">
+		<h4>2022</h4>
+		<h3>Graduation</h3>
+		<p>Oxford English Higher Secondary School</p>
+	</div>
+</div>
+<div class="mb-4"></div>
+<h2>Contact Us</h2>
+<div class="mb-4"></div>
+<form>
+<div class="rows">
+	<div class="input-group">
+		<input type="text" id="name" required>
+		<label for="name"><i class="bi bi-person-fill"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+  			<path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+			</svg> Name</label>
+	</div>
+
+	<div class="input-group">
+		<input type="text" id="number" required>
+		<label for="number"><i class="bi bi-telephone-fill"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+  		<path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+		</svg> Phone No</label>
+	</div>
+</div>
+
+	<div class="input-group">
+		<input type="email" id="email" required>
+		<label for="email"><i class="bi bi-envelope-fill"></i> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+  		<path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
+		</svg> Email Id</label>
+	</div>
+
+	<div class="input-group">
+		<textarea id="message" rows="8" required></textarea>
+		<label for="message"><i class="bi bi-chat-right-text-fill"></i> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-right-text-fill" viewBox="0 0 16 16">
+  		<path d="M16 2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h9.586a1 1 0 0 1 .707.293l2.853 2.853a.5.5 0 0 0 .854-.353V2zM3.5 3h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1 0-1zm0 2.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1 0-1zm0 2.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1z"/>
+		</svg> Message</label>
+	</div>
+
+	<button>Submit</button>
+</form>
+
+
+	
+</section>
+	<script src="script.js"></script>
+
+</body>
+</html>
